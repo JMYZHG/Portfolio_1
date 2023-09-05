e9aeeb0cad0371f2fd8b2cb930202169e2fe2045
JimmyZhang <jimmyzhang.business@gmail.com>
2023-09-05 13:13:39 -0400

    edited animation JS

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const observe = document.querySelectorAll('.anim');
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } 
-    });
-});
+observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        // Once its in view, add animation style
+        if(entry.intersectionRatio > 0) {
+            entry.target.getElementsByClassName.animation = `anim1 2s forwards ease-out`;
+        } else {
+            entry.target.style.animation = 'none';
+        }
+    })
+})
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) => observer.observe(el));
+images.forEach(image => {
+    observer.observe(image)
+})
 
