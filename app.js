8c9576c0f2da337a65e8422dd3bbeb1d21f8e63d
JimmyZhang <jimmyzhang.business@gmail.com>
2023-09-09 15:47:14 -0400

    added SASS file and updated animations

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const observe = document.querySelectorAll('.anim');
+// RESUME BUTTON
+function openResume() {
+    
+    const pdfFile = 'images/Resume.pdf';
+
+    // Open the PDF file in a new tab
+    window.open(pdfFile, '_blank');
+}
+
+
+const images = document.querySelectorAll('.anim');
 
 observer = new IntersectionObserver((entries) => {
+
     entries.forEach(entry => {
-        // Once its in view, add animation style
         if(entry.intersectionRatio > 0) {
-            entry.target.getElementsByClassName.animation = `anim1 2s forwards ease-out`;
-        } else {
-            entry.target.style.animation = 'none';
+            entry.target.style.animation = `anim1 .5s ${entry.target.dataset.delay} forwards ease-out`;
         }
+        
     })
+
 })
 
 images.forEach(image => {
     observer.observe(image)
 })
+
 
